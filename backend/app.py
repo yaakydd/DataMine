126dbbed476813688a340191f7e35ebd3a315565
Yaa <antwiyaa17@gmail.com>
2026-02-18 05:05:14 +0000

    Added the clean router

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
 from routes.data_Info import data_info
+from routes.data_Cleaning import clean
 
 app = FastAPI(title="DataMine API")
 
 app.include_router(data_info, prefix="/api", tags=["dataset_info"])
+app.include_router(clean, prefix="/cleaningAPI", tags=["dataset_cleaning"])
 
 @app.get("/")
 async def root():
