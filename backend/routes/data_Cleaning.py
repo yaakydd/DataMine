dccb7c4c4683d4a4b185670d37dcc596f727619c
Yaa <antwiyaa17@gmail.com>
2026-02-18 04:59:28 +0000

    Checks for the names and column datatypes using the dtypes function

--- backend/routes/data_Cleaning.py
+++ backend/routes/data_Cleaning.py
@@ -5,9 +5,23 @@ from routes.dfState import dataset_state
 
 clean = APIRouter()
 
+
 @clean.post("/dataset_cleaning")
-async def cleaning():
-    return
+#Function to handle wrong datatypes
+async def wrong_datatypes_cleaning():
+    if dataset_state.df is None:
+        raise HTTPException(status_code=400, detail="Dataset isn't loaded and stored")
+
+    try:   
+        clean_df = dataset_state.df
+        #Identifying the column datatypes
+        column_datatypes = clean_df.dtypes
+        return {
+            'Column_Datatpes' : column_datatypes
+        }
+    except Exception as e:
+        raise HTTPException(status_code=400)
+        
 
 
 
