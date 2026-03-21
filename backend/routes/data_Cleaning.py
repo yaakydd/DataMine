4adaae36676d522a309e43046535dc9f871e241e
Yaa <antwiyaa17@gmail.com>
2026-03-21 05:32:03 +0000

    Ready to implement the various data cleaning techniques

--- backend/routes/data_Cleaning.py
+++ backend/routes/data_Cleaning.py
@@ -3,9 +3,7 @@ from .dfState import dataset_state
 import pandas as pd
 import re
 
-
 clean = APIRouter()
-
 
 @clean.post("/dataset_cleaning")
 #Function to handle wrong datatypes
@@ -19,7 +17,7 @@ async def wrong_datatypes_cleaning():
       # This is how it works: Goes through the list one-by-one(.apply(...)), A placeholder that says: "For each item in the list, call it 'x' for a second." lambda x: and The action: "Look at item 'x' and grab only its 'name' property." (x.name) only refers to the values{int64...} not the labels{column_name}.
         column_datatypes = clean_df.dtypes.apply(lambda x: x.name).to_dict()
         return {
-            'Column_Datatpes' : column_datatypes
+            'Column_Datatypes' : column_datatypes
         }
     except Exception as e:
         raise HTTPException(status_code=400)
--- backend/routes/dfState.py
+++ backend/routes/dfState.py
@@ -1,11 +1,11 @@
 from typing import Optional
 import pandas as pd
 
-#Creating the dataset State class (plan)
+#Creating the dataset State class 
 class dfState:
     # Using the class constructor to setup the dataframe
     def __init__(self):
-        self.df: Optional[pd.DataFrame] = None  # This will holds the DataFrame globally but for now its empty
+        self.df: Optional[pd.DataFrame] = None  # This will hold the DataFrame globally but for now its empty
 
-# Create one instance to be imported everywhere across all the endpoints
+# Created one instance to be imported everywhere across all the endpoints
 dataset_state = dfState()
